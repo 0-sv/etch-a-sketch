e911f55506f0e080d1496f999fb90cafd79ef6db
Sebastiaan Verbeek <contact@svatwork.com>
2020-04-23 13:18:31 +0700

    Put divs in a container div

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-document.body = document.createElement('body');
-const body = document.querySelector('body');
 const l = w = 4;
 
-const createField = (l, w) => {
+document.body = document.createElement('body');
+const body = document.querySelector('body');
+
+const container = document.createElement('div');
+container.classList.add('container');
+body.appendChild(container);
+
+const createField = (l, w, container) => {
     for (let i = 0; i < l * w; ++i) {
         let node = document.createElement('div');
-        node.classList.add('square')
-        body.appendChild(node); 
+        node.classList.add('square');
+        container.appendChild(node); 
     }
 }
 
-createField(l, w);
+createField(l, w, container);
 console.log(body);
